67b2038c9afe00b383df74b0d9d9de2522f72945
dog <olegduletsky@gmail.com>
2021-02-24 00:44:58 +0200

    fix - Lemma - spec

--- spec/models/lemma_spec.rb
+++ spec/models/lemma_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Lemma, type: :model do
   let(:lemma) { create :lemma }
 
-  it('build') { expect(build(:lemma, kind_id)).to be_valid() }
+  it('build') { expect(build(:lemma)).to be_valid() }
   it('create') { expect(create(:lemma)).to be_valid() }
 
 end
