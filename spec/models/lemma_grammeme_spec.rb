8cc06c39300ef0f91d0dc653160fc06040505bc3
dog <olegduletsky@gmail.com>
2021-02-24 00:42:34 +0200

    fix - LemmaGrammeme - spec

--- spec/models/lemma_grammeme_spec.rb
+++ spec/models/lemma_grammeme_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Grammeme, type: :model do
-  let(:lemma_grammeme) { create :grammeme }
+RSpec.describe LemmaGrammeme, type: :model do
+  let(:grammeme) { create :grammeme }
+  let(:lemma_form) { create :lemma_form }
+  let(:lemma_grammeme) { create :lemma_grammeme, kind_id: lemma_form.id, grammeme_id: grammeme.id }
 
-  it('build') { expect(build(:lemma_grammeme, kind_id)).to be_valid() }
-  it('create') { expect(create(:lemma_grammeme)).to be_valid() }
+  it('build') { expect(build(:lemma_grammeme)).to be_valid() }
+  it('create') { expect(lemma_grammeme).to be_valid() }
 
 end
