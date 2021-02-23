845466b4abead3e62207e3d81b5772989f87159b
dog <olegduletsky@gmail.com>
2021-02-24 00:49:49 +0200

    fix - rake import:xml - spec

--- spec/tasks/import_spec.rb
+++ spec/tasks/import_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'rake import:xml', type: :task do
+  let(:text) { /Xml imported/ }
 
   context 'when no data' do
-    let(:text) { /Xml imported/ }
-
     it { expect { task.execute }.to output(text).to_stdout }
   end
 
   context 'when data exists' do
-    let(:text) { 'бежала'  }
     let(:params) { { form: text } }
     let(:grammema) { create :grammeme }
     let(:lemma_form) { create :lemma_form, text: text }
-    let(:lemma_grammeme) { create :lemma_grammeme, kind_type: 'LemmaForm', kind_id: lemma_form.id, grammeme_id: grammema.id }
+    let(:lemma_grammeme) do
+      create :lemma_grammeme,
+             kind_type: 'LemmaForm',
+             kind_id: lemma_form.id,
+             grammeme_id: grammema.id
+    end
 
     before { lemma_grammeme }
 
