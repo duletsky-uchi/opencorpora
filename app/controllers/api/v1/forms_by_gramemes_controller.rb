b42d505be46c6d63149a40bbe00a9d63906650f6
dog <olegduletsky@gmail.com>
2021-02-27 15:25:33 +0200

    spec - /api/v1/forms_by_gramemes - Получить по грамемам словоформы

--- app/controllers/api/v1/forms_by_gramemes_controller.rb
+++ app/controllers/api/v1/forms_by_gramemes_controller.rb
@@ -11,8 +11,10 @@ class Api::V1::FormsByGramemesController < Api::V1::BaseController
   #  {"grammemes":[{"lemma_id":237957,"name":"femn","alias":"жр","description":"женский род"},{"lemma_id":237957,"name":"sing","alias":"ед","description":"единственное число"},{"lemma_id":237957,"name":"past","alias":"прош","description":"прошедшее время"},{"lemma_id":237957,"name":"indc","alias":"изъяв","description":"изъявительное наклонение"},{"lemma_id":237962,"name":"femn","alias":"жр","description":"женский род"},{"lemma_id":237962,"name":"sing","alias":"ед","description":"единственное число"},{"lemma_id":237962,"name":"past","alias":"прош","description":"прошедшее время"},{"lemma_id":237962,"name":"indc","alias":"изъяв","description":"изъявительное наклонение"}]}%
   def show
     return err(:empty_grammemes) if params[:grammemes].empty?
+    return err(:empty_grammemes) if grammemes.empty?
 
-    sql = Lemma.send(:sanitize_sql_array, [sql_template, params[:grammemes].split(/\s*,\s*/)])
+    pp "#{__FILE__}, #{__LINE__} | grammemes=#{grammemes.inspect}"
+    sql = Lemma.send(:sanitize_sql_array, [sql_template, grammemes])
     out = ActiveRecord::Base.connection.select_all(sql)
 
     render json: { forms: out }
@@ -30,5 +32,11 @@ class Api::V1::FormsByGramemesController < Api::V1::BaseController
       AND g.name IN (?)
       LIMIT 10
     SQL
+  end
+
+  private
+
+  def grammemes
+    @grammemes ||= params[:grammemes].split(/\s*,\s*/)
   end
 end
